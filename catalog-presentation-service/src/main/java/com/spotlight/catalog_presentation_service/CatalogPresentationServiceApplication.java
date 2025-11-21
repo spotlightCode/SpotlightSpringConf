a6c964e8030b317e87af0ffacf7bed14bbc9640e
idriss Tagny <idrisstagny@gmail.com>
2025-11-21 06:55:33 +0100

    feat: ajouter les services initiaux et la configuration centrale
    Ajout des services de base pour le projet (configuration, registre de services,
    présentation du catalogue) ainsi que les wrappers Maven et les propriétés de
    configuration centralisée.

---
+++ catalog-presentation-service/src/main/java/com/spotlight/catalog_presentation_service/CatalogPresentationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spotlight.catalog_presentation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogPresentationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogPresentationServiceApplication.class, args);
+	}
+
+}
---
+++ catalog-presentation-service/src/test/java/com/spotlight/catalog_presentation_service/CatalogPresentationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotlight.catalog_presentation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogPresentationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
