1b6e2402a44cff1a73528f500341ac66d7275c89
idriss Tagny <idrisstagny@gmail.com>
2025-11-25 07:20:33 +0100

    Add proxy service module with Spring Boot configuration
    - Initialize proxy-service as new Maven module
    - Configure application properties and cloud config
    - Set up project structure with main application class and tests

---
+++ proxy-service/src/test/java/spotlight/proxy_service/ProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spotlight.proxy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
